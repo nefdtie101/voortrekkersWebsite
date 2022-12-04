2a66cc0a1816a6cfe4026dd3f9bdb8a081a34f2c
Johann Nefdt <58251579+nefdtie101@users.noreply.github.com>
2022-12-04 17:56:14 +0200

    first commit

---
+++ Component/nav.js
@@ -0,0 +1,16 @@
+import { Navbar, Nav } from 'rsuite';
+
+
+export default function NavBar(){
+    return (
+        <div>
+            <Navbar>
+                <Nav>
+                    <Nav.Item eventKey="1">
+                        Home
+                    </Nav.Item>
+                </Nav>
+            </Navbar>
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import 'rsuite/dist/rsuite.min.css';
+import '../styles/globals.css';
+import NavBar from "../Component/nav";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <div>
+        <NavBar/>
+        <Component {...pageProps} />
+      </div>
+      )
 }
 
 export default MyApp
