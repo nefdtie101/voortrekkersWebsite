c05705f18d9a117a8685d4bbd32855943a9a3714
Johann Nefdt <johann@nefdtco.co.za>
2022-12-20 17:01:18 +0000

    fixed url

--- .space.kts
+++ .space.kts
@@ -4,11 +4,6 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
-    container(displayName = "Say Hello", image = "hello-world")
-}
-
-
 job("Build and push Docker") {
     host("Build artifacts and a Docker image") {
         // generate artifacts required for the image
@@ -21,7 +16,7 @@ job("Build and push Docker") {
         dockerBuildPush {
             context = "docker"
             labels["vendor"] = "mycompany"
-            val spaceRepo = "docker build -t nefdtco.registry.jetbrains.space/p/voortrekkers/docker/voortrekkersWebsite"
+            val spaceRepo = "nefdtco.registry.jetbrains.space/p/voortrekkers/docker/voortrekkersWebsite"
             // image tags for 'docker push'
             tags {
                 +"$spaceRepo:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
