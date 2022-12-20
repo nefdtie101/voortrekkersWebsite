6f7c48b24947d6ca0d5eb79fa5634bcfa290b2b0
Johann Nefdt <johann@nefdtco.co.za>
2022-12-20 17:35:52 +0000

    test

--- .space.kts
+++ .space.kts
@@ -15,7 +15,7 @@ job("Build and push Docker") {
             // Docker context, by default, project root
             // path to Dockerfile relative to project root
             // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
-             file = "/Dockerfile"
+             file = "Dockerfile"
          
 
             val spaceRepo = "nefdtco.registry.jetbrains.space/p/voortrekkers/docker/voortrekkerwebsite"
